a215857088410af2cd94df138e2ada6983915154
nutboltz <eunicehx920@gmail.com>
2024-06-02 15:38:25 -0400

    vercel url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,9 +5,12 @@ export function getBaseUrl(): string {
     if (typeof window !== "undefined")
       // browser should use relative path
       return "";
-    if (process.env.VERCEL_URL)
+    if (process.env.VERCEL_URL)  
       // reference for vercel.com
       return `https://${process.env.VERCEL_URL}`;
+    if (process.env.NEXT_PUBLIC_VERCEL_URL)  
+        // reference for vercel.com
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
     // assume localhost
     return `http://localhost:${process.env.PORT ?? 3000}`;
   }
@@ -16,6 +19,9 @@ export function getBaseUrl(): string {
     if (process.env.VERCEL_URL)
       // reference for vercel.com
       return `https://${process.env.VERCEL_URL}`;
+    if (process.env.NEXT_PUBLIC_VERCEL_URL)  
+        // reference for vercel.com
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
     // assume localhost
     return `http://localhost:${process.env.PORT ?? 3000}`;
   }

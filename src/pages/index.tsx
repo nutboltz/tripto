f0091584664c349797b465509a8a00297cbf8587
Aja Singer <ajasinger@gmail.com>
2024-06-01 22:16:30 -0400

    import inter & add styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import CreateTripForm from '@/components/createTripForm';
 import { useRouter } from 'next/navigation';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
@@ -11,9 +14,10 @@ export default function Home() {
 
   return (
     <main>
-      <div className="flex gap-16">
-        {/* populate trips on left side */}
-        <div>trips</div>
+      <div className={`p-20 flex gap-16 relative ${inter.className}`}>
+        <div className="bg-[#D5AEE4] opacity-15 absolute -top-[10rem] right-[0rem] h-[40rem] w-[40rem] rounded-full blur-[7rem] z-0"></div>
+        <div className="bg-[#AEC7E4] opacity-15 absolute -bottom-[10rem] left-[0rem] h-[75rem] w-[75rem] rounded-full blur-[7rem] z-0"></div>
+        <div>Your trip</div>
         < CreateTripForm onSubmit={onSubmit}/>
       </div>
     </main>

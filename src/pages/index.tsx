1568dedf4be9551a962bdece376d0cb8a27f746f
Aja Singer <ajasinger@gmail.com>
2024-06-01 23:21:25 -0400

    styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,11 +14,13 @@ export default function Home() {
 
   return (
     <main>
-      <div className={`p-20 flex gap-16 relative ${inter.className}`}>
-        <div className="bg-[#D5AEE4] opacity-15 absolute -top-[10rem] right-[0rem] h-[40rem] w-[40rem] rounded-full blur-[7rem] z-0"></div>
-        <div className="bg-[#AEC7E4] opacity-15 absolute -bottom-[10rem] left-[0rem] h-[75rem] w-[75rem] rounded-full blur-[7rem] z-0"></div>
-        <div>Your trip</div>
-        < CreateTripForm onSubmit={onSubmit}/>
+      <div className={`py-20 px-32 flex flex-col gap-10 relative ${inter.className}`}>
+        <div className="bg-[#D5AEE4] opacity-15 absolute -top-[10rem] -right-[10rem] h-[40rem] w-[40rem] rounded-full blur-[7rem] z-0"></div>
+        <div className="bg-[#AEC7E4] opacity-15 absolute top-[20rem] -left-[10rem] h-[75rem] w-[75rem] rounded-full blur-[7rem] z-0"></div>
+        <h1 className="font-semibold text-4xl z-10">Your trip</h1>
+        <div className="z-10">
+          < CreateTripForm onSubmit={onSubmit}/>
+        </div>
       </div>
     </main>
   );
